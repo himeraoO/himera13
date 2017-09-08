e2f56d90f968f8960a3063f71d9ce10500d87e8a
himeraoO <himer@bk.ru>
2017-09-09 00:28:03 +0300

    First commit

--- src/Blog.java
+++ src/Blog.java
@@ -1,52 +1,29 @@
 import java.util.Date;
-
 public class Blog {
     public static void main(String[] args) {
-
-//Post post = new Post(1, "first", "first1");
-//        System.out.println(post.getPostId());
-//        System.out.println(post.getTitle());
-//        System.out.println(post.getText());
-//       System.out.println(post.getPostedAt());
-
-
-        Date data1 = new Date();
-        long x1 = data1.getTime();
-
-        Date data2 = new Date();
-        long x2 = data2.getTime();
-
-        Date data3 = new Date();
-        long x3 = data3.getTime();
-
-        Date data4 = new Date();
-        long x4 = data4.getTime();
-
-        Date data5 = new Date();
-        long x5 = data5.getTime();
-
-        Date data6 = new Date();
-        long x6 = data6.getTime();
-
-        Date data7 = new Date();
-        long x7 = data7.getTime();
-
         Post[] posts = new Post[7];
-        posts[0] = new Post(24,"Night", "Hello", data1);
-        posts[1] = new Post(23,"Day" ,"Yes", data2);
-        posts[2] = new Post(2, "Star", "Smile", data3);
-        posts[3] = new Post(5, "World", "Tea", data4);
-        posts[4] = new Post(16, "Moon", "Carrot", data5);
-        posts[5] = new Post(42, "Pool", "Apple", data6);
-        posts[6] = new Post(8, "Card", "Hats", data7);
-
-//        for (Post b: posts)
-//            System.out.println(b);
+        posts[0] = new Post(24,"Night", "Hello", new Date());
+        posts[1] = new Post(23,"Day" ,"Yes", new Date());
+        posts[2] = new Post(2, "Star", "Smile", new Date());
+        posts[3] = new Post(5, "World", "Tea", new Date());
+        posts[4] = new Post(16, "Moon", "Carrot", new Date());
+        posts[5] = new Post(42, "Pool", "Apple", new Date());
+        posts[6] = new Post(8, "Card", "Hats", new Date());
         for (Post a: posts)
-        System.out.println(a.getPostId() +" " + a.getTitle() + " " + a.getText() + " " + a.getPostedAt());
-
-
-
-
+            System.out.println(a.getPostId() +" " + a.getTitle() + " " + a.getText() + " " + a.getPostedAt());
+        for (int i = 0; i < posts.length; i++) {
+            for (int j = i + 1; j < posts.length; j++) {
+                if (posts[i].getPostId() > posts[j].getPostId()) {
+//              if (posts[i].getTitle().compareTo(posts[j].getTitle()) < 0) {
+                    Post temp = posts[j];
+                    posts[j] = posts[i];
+                    posts[i] = temp;
+                }
+            }
+        }
+        for(Post post : posts) {
+            System.out.println(post.getPostId() + "");
+//            System.out.println(post.getTitle() + "");
+        }
     }
 }
--- src/Post.java
+++ src/Post.java
@@ -1,49 +1,39 @@
 import java.util.Date;
-
 public class Post {
         private long postId;
         private String title;
         private String text;
         private Date postedAt;
-
-        Post(long postId, String title, String text, Date postedAt ) {
+    public Post() {
+    }
+    public Post(long postId, Date postedAt) {
+        this.postId = postId;
+        this.postedAt = postedAt;
+    }
+    public Post(long postId, String title, String text, Date postedAt ) {
             this.postId = postId;
             this.title = title;
             this.text = text;
             this.postedAt = postedAt;
-
-        }
-
-        public long getPostId() {
-
-            return postId;
-        }
-
-        public String getTitle() {
+    }
+    public long getPostId() { return postId; }
+    public String getTitle() {
             return title;
         }
-
-        public String getText() {
+    public String getText() {
             return text;
         }
-
-        public Date getPostedAt() {
+    public Date getPostedAt() {
             return postedAt;
         }
-
-        public void setPostId(long postId) {
-            this.postId = postId < 0 ? 0 : postId;
-
-        }
-
-        public void setTitle(String title) {
+    public void setPostId(long postId) { this.postId = postId < 0 ? 0 : postId; }
+    public void setTitle(String title) {
             this.title = title;
         }
-
-        public void setText(String text) {
+    public void setText(String text) {
             this.text = text;
         }
-        public void setPostedAt(Date postedAt){
+    public void setPostedAt(Date postedAt){
             this.postedAt = postedAt;
         }
 }
