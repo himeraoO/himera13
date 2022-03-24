3e697b120153854ac3dff3093171b19ff013f0f8
himeraoO <31455074+himeraoO@users.noreply.github.com>
2022-03-24 20:46:00 +0300

    Delete src/newpaket2 directory

--- src/newpaket2/structure/CustomList.java
+++
@@ -1,9 +0,0 @@
-package newpaket2.structure;
-
-public interface CustomList<T> {
-
-    void add(T value);
-
-    void delete(int index);
-
-}
--- src/newpaket2/structure/EmptyQueueException.java
+++
@@ -1,9 +0,0 @@
-package newpaket2.structure;
-
-public class EmptyQueueException extends RuntimeException {
-
-    public EmptyQueueException(String message){
-        super(message);
-    }
-
-}
